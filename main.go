b9243dfc314ca7a498e74e127fc7519ab798b617
RianIhsan <Rianwhatsup@gmail.com>
2023-11-06 23:36:46 +0700

    feature:get products

--- main.go
+++ main.go
@@ -2,11 +2,18 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/capstone-kelompok-7/backend-disappear/config"
 	"github.com/capstone-kelompok-7/backend-disappear/middlewares"
 	hAuth "github.com/capstone-kelompok-7/backend-disappear/module/auth/handler"
 	rAuth "github.com/capstone-kelompok-7/backend-disappear/module/auth/repository"
 	sAuth "github.com/capstone-kelompok-7/backend-disappear/module/auth/service"
+	"github.com/capstone-kelompok-7/backend-disappear/module/product/handler"
+	"github.com/capstone-kelompok-7/backend-disappear/module/product/repository"
+	"github.com/capstone-kelompok-7/backend-disappear/module/product/service"
+
+	"net/http"
+
 	hUser "github.com/capstone-kelompok-7/backend-disappear/module/users/handler"
 	rUser "github.com/capstone-kelompok-7/backend-disappear/module/users/repository"
 	sUser "github.com/capstone-kelompok-7/backend-disappear/module/users/service"
@@ -15,7 +22,6 @@ import (
 	"github.com/capstone-kelompok-7/backend-disappear/utils/database"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
-	"net/http"
 )
 
 func main() {
@@ -25,14 +31,19 @@ func main() {
 	db := database.InitDatabase(*initConfig)
 	database.Migrate(db)
 	jwtService := utils.NewJWT(initConfig.Secret)
+	hash := utils.NewHash()
 
 	userRepo := rUser.NewUserRepository(db)
 	userService := sUser.NewUserService(userRepo)
 	userHandler := hUser.NewUserHandler(userService)
 
 	authRepo := rAuth.NewAuthRepository(db)
-	authService := sAuth.NewAuthService(authRepo, jwtService, userService)
+	authService := sAuth.NewAuthService(authRepo, jwtService, userService, hash)
 	authHandler := hAuth.NewAuthHandler(authService, userService)
+
+	productRepo := repository.NewProductRepository(db)
+	productService := service.NewProductService(productRepo)
+	productHandler := handler.NewProductHandler(productService)
 
 	e.Pre(middleware.RemoveTrailingSlash())
 	e.Use(middleware.CORS())
@@ -43,5 +54,6 @@ func main() {
 	})
 	routes.RouteUser(e, userHandler)
 	routes.RouteAuth(e, authHandler)
+	routes.RouteProduct(e, productHandler)
 	e.Logger.Fatalf(e.Start(fmt.Sprintf(":%d", initConfig.ServerPort)).Error())
 }
