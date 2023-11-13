673fa8ebe05060c63d48980a2341c7c7734b1433
RianIhsan <Rianwhatsup@gmail.com>
2023-11-13 21:50:17 +0700

    improvement:email-template

--- utils/email/email.go
+++ utils/email/email.go
@@ -1,23 +1,28 @@
 package email
 
 import (
+	"errors"
 	"github.com/wneessen/go-mail"
+	"html/template"
 	"os"
+	"path/filepath"
+	"runtime"
 	"strconv"
+	"strings"
 )
 
-func EmaiilService(email, otp string) error {
-	secret_user := os.Getenv("SMTP_USER")
-	secret_pass := os.Getenv("SMTP_PASS")
-	secret_port := os.Getenv("SMTP_PORT")
+func EmailService(email, otp string) error {
+	secretUser := os.Getenv("SMTP_USER")
+	secretPass := os.Getenv("SMTP_PASS")
+	secretPort := os.Getenv("SMTP_PORT")
 
-	convPort, err := strconv.Atoi(secret_port)
+	convPort, err := strconv.Atoi(secretPort)
 	if err != nil {
 		return err
 	}
 
 	m := mail.NewMsg()
-	if err := m.From(secret_user); err != nil {
+	if err := m.From(secretUser); err != nil {
 		return err
 	}
 	if err := m.To(email); err != nil {
@@ -25,9 +30,34 @@ func EmaiilService(email, otp string) error {
 	}
 
 	m.Subject("Verifikasi Email - Disappear Organization")
-	m.SetBodyString(mail.TypeTextPlain, "Kode OTP anda adalah : "+otp)
+	emailTemplate := struct {
+		OTP   string
+		Email string
+	}{
+		OTP:   otp,
+		Email: email,
+	}
 
-	c, err := mail.NewClient("smtp.gmail.com", mail.WithPort(convPort), mail.WithSMTPAuth(mail.SMTPAuthPlain), mail.WithUsername(secret_user), mail.WithPassword(secret_pass))
+	_, filename, _, ok := runtime.Caller(0)
+	if !ok {
+		return errors.New("Failed to get the current file path")
+	}
+
+	templatePath := filepath.Join(filepath.Dir(filename), "template.html")
+
+	tmpl, err := template.New("emailTemplate").ParseFiles(templatePath)
+	if err != nil {
+		return err
+	}
+
+	var bodyContent strings.Builder
+	if err := tmpl.ExecuteTemplate(&bodyContent, "template.html", emailTemplate); err != nil {
+		return err
+	}
+
+	m.SetBodyString(mail.TypeTextHTML, bodyContent.String())
+
+	c, err := mail.NewClient("smtp.gmail.com", mail.WithPort(convPort), mail.WithSMTPAuth(mail.SMTPAuthPlain), mail.WithUsername(secretUser), mail.WithPassword(secretPass))
 	if err != nil {
 		return err
 	}
